0862466234de4fcdd6f54723e1142df5a275a227
Bobby Rockers <brockers@bisonok.com>
2019-09-14 11:39:11 -0500

    Testing all th eroutes

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+//My App
 var express = require('express');
 var app = express();
 var port =  3000;
 
 app.get('/helloworld', function(req,res){
     res.send("Hello to the world.");
+});
+
+app.post('/helloworld', function(req,res){
+    res.send("This is NOT the right Hello World!");
+});
+
+app.get('/hello/world', function(req,res){
+    res.send("This is ANOTHER hello world.");
 });
 
 app.listen(port, function(){
