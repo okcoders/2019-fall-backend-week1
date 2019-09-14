8372f0709413bc5e6a658ee3acea84d7a0aa2133
Bobby Rockers <brockers@bisonok.com>
2019-09-14 12:51:26 -0500

    Static file serving and AJAX requests.

--- app.js
+++ app.js
@@ -3,23 +3,39 @@ var express = require('express');
 var app = express();
 var port =  3000;
 
-app.get(
-    '/helloworld', 
-    function(req,res){
-        res.send("<h1>Hello to the world.</h1>"); 
-    }
+app.use('/', express.static('files'));
+
+app.get( '/helloworld', function(req,res){ 
+  res.send("<h1>Hello to the world.</h1>"); }
 );
 
 app.post('/helloworld', function(req,res){
-    res.send("This is NOT the right Hello World!");
+ res.send("This is NOT the right Hello World!");
 });
 
 app.get('/json/helloworld', function(req,res){
-    res.json( { message: "Hello to the world." } );
+ res.json( { message: "Hello to the world." } );
 });
 
 app.get('/hello/world', function(req,res){
-    res.send("This is ANOTHER hello world.");
+ res.send("This is ANOTHER hello world.");
+});
+
+app.get('/message/1', function(req,res){
+	res.json( {message: "This is message one"} );
+});
+
+app.get('/message/1.thing', function(req,res){
+	res.json( {message: "This is message one dot thing"} );
+});
+
+app.get('/message/for/:id', function(req,res){
+	var userID = req.params.id;
+	res.json( {message: "This is a message for " + userID} );
+});
+
+app.get('/message/*', function(req,res){
+	res.json( {message: "THis is every other message"} );
 });
 
 app.listen(port, function(){
