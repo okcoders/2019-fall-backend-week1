8423b4a18e42330923be1f0c1817bceb8f11f59d
Bobby Rockers <brockers@bisonok.com>
2019-09-14 12:17:09 -0500

    setting up our dev environment.

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ var express = require('express');
 var app = express();
 var port =  3000;
 
-app.get('/helloworld', function(req,res){
-    res.send("Hello to the world.");
-});
+app.get(
+    '/helloworld', 
+    function(req,res){
+        res.send("<h1>Hello to the world.</h1>"); 
+    }
+);
 
 app.post('/helloworld', function(req,res){
     res.send("This is NOT the right Hello World!");
+});
+
+app.get('/json/helloworld', function(req,res){
+    res.json( { message: "Hello to the world." } );
 });
 
 app.get('/hello/world', function(req,res){
@@ -16,5 +23,5 @@ app.get('/hello/world', function(req,res){
 });
 
 app.listen(port, function(){
-    console.log("Express running on port" + port);
+    console.log("Express running on port:" + port);
 });
